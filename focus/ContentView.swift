5125609790ce3d3fd1225bf943d9629be8daaa45
Leonid Perlin <perlinleo@gmai.com>
2025-08-04 12:40:01 +0300

    Initial Commit

---
+++ focus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  focus
+//
+//  Created by blacksnow on 8/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ focus/focusApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusApp.swift
+//  focus
+//
+//  Created by blacksnow on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct focusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
