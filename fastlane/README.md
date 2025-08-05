1adf453df4eeb85b588992d9efacd047727a4b7a
Leonid Perlin <perlinleo@gmai.com>
2025-08-05 12:11:22 +0300

    Added fastlane

---
+++ fastlane/README.md
@@ -0,0 +1,72 @@
+fastlane documentation
+----
+
+# Installation
+
+Make sure you have the latest version of the Xcode command line tools installed:
+
+```sh
+xcode-select --install
+```
+
+For _fastlane_ installation instructions, see [Installing _fastlane_](https://docs.fastlane.tools/#installing-fastlane)
+
+# Available Actions
+
+## iOS
+
+### ios tests
+
+```sh
+[bundle exec] fastlane ios tests
+```
+
+
+
+### ios register_app
+
+```sh
+[bundle exec] fastlane ios register_app
+```
+
+
+
+### ios get_dev_certs
+
+```sh
+[bundle exec] fastlane ios get_dev_certs
+```
+
+
+
+### ios sync_all_development
+
+```sh
+[bundle exec] fastlane ios sync_all_development
+```
+
+
+
+### ios sync_device_info
+
+```sh
+[bundle exec] fastlane ios sync_device_info
+```
+
+
+
+### ios beta
+
+```sh
+[bundle exec] fastlane ios beta
+```
+
+
+
+----
+
+This README.md is auto-generated and will be re-generated every time [_fastlane_](https://fastlane.tools) is run.
+
+More information about _fastlane_ can be found on [fastlane.tools](https://fastlane.tools).
+
+The documentation of _fastlane_ can be found on [docs.fastlane.tools](https://docs.fastlane.tools).
--- focus/ContentView.swift
+++ focus/ContentView.swift
@@ -7,18 +7,108 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    private enum TimerStatus {
+        case timerFocus
+        case timerBreak
+        case timerIdle
+        
+        var title: String {
+            switch self {
+                case .timerIdle:
+                    return "Timer is Idle"
+                case .timerFocus:
+                    return "Focus time"
+                case .timerBreak:
+                    return "Break time"
+            }
+        }
+    }
+    
+    @State var timeRemaining: CGFloat = 10
+    let timer = Timer.publish(
+        every: 1 / Constants.frameRate,
+        on: .main,
+        in: .common
+    ).autoconnect()
+    
+    @State private var currentStatus: TimerStatus = .timerIdle
+    
+    private let formatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.minute, .second]
+        formatter.zeroFormattingBehavior = [.pad]
+        
+        return formatter
+    }()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text(currentStatus.title).font(.title)
+            Spacer()
+            
+            if currentStatus != .timerIdle {
+                Text(formatter.string(from: timeRemaining) ?? "00:00").font(.largeTitle)
+                    .onReceive(timer, perform: {
+                        _ in
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1 / Constants.frameRate
+                        } else {
+                            stopAll()
+                        }
+                })
+                
+                Spacer()
+            }
+            
+            switch currentStatus {
+            case .timerFocus:
+                Button(action: startBreak) {
+                    Text("Start Break")
+                }
+                Button(action: stopAll) {
+                    Text("Stop all")
+                }
+            case .timerIdle:
+                Button(action: startFocus) {
+                    Text("Start Focus")
+                }
+            case .timerBreak:
+                Button(action: startFocus) {
+                    Text("Start Focus")
+                }
+                Button(action: stopAll) {
+                    Text("Stop all")
+                }
+            }
+            Spacer()
         }
         .padding()
+    }
+    
+    private func startFocus() {
+        currentStatus = .timerFocus
+        timeRemaining = Constants.focusTime
+    }
+    
+    private func startBreak() {
+        currentStatus = .timerBreak
+        timeRemaining = Constants.breakTime
+    }
+    
+    private func stopAll() {
+        currentStatus = .timerIdle
     }
 }
 
 #Preview {
     ContentView()
+}
+
+private enum Constants {
+    static let frameRate: CGFloat = 120
+    static let focusTime: CGFloat = 60 * 25
+    static let breakTime: CGFloat = 60 * 5
 }
